daa91bb59ed9f208bbf6ec26fad510eb5a3bd503
Jordan Hudgens <jordan@devcamp.com>
2017-07-18 16:43:58 +0000

    Integrated new gem for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end 
   end
+  
+  def copyright_generator
+    MorrisViewTool::Renderer.copyright 'Thomas Morris', 'All rights reserved'
+  end
 end
